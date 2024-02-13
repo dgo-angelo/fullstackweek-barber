22624b875728435a8a89694819c522abdc2cf65d
Diego de Angelo <dgo.angelo@gmail.com>
2024-02-13 19:00:00 -0200

    (fix) add Service type

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from '@/app/_components/ui/button';
 import { db } from '@/app/_lib/prisma';
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react';
-import Image from 'next/image';
+
 import BarbershopInfo from './_components/barbershop-info';
 import ServiceItem from './_components/service-item';
+import { Service } from '@prisma/client';
 
 interface BarbershopDetailsPageProp {
   params: {
@@ -33,7 +32,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProp) => {
     <div>
       <BarbershopInfo barbershop={barbershop} />
       <div className='flex flex-col gap-4 px-5 py-6'>
-        {barbershop.services.map((service) => (
+        {barbershop.services.map((service: Service) => (
           <ServiceItem service={service} key={service.id} />
         ))}
       </div>
