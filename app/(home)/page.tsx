fd342a04b4d367492c7222d9ea6a111068a68974
Diego de Angelo <dgo.angelo@gmail.com>
2024-02-13 19:00:00 -0200

    (feat) add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -32,6 +32,14 @@ export default async function Home() {
           ))}
         </div>
       </div>
+      <div className='mt-6 mb-[4.5rem]'>
+        <h2 className='px-5 text-xs uppercase text-gray-400 font-bold mb-3'>Populares</h2>
+        <div className='flex gap-2 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+          {barbershops.map((barbershop) => (
+            <BarbershopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
