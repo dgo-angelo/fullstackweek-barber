ef8d17bb7cbf4225f7e2ff3d6295467448c4f574
Diego de Angelo <dgo.angelo@gmail.com>
2024-02-13 19:00:00 -0200

    (feat) add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import Header from '../_components/header';
 import { ptBR } from 'date-fns/locale';
+import Search from './_components/search';
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <div className='px-5 pt-5'>
         <h2 className='text-xl font-bold'>Olá, Diego</h2>
         <p className='capitalize'>{format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}</p>
+      </div>
+      <div className='px-5 mt-6'>
+        <Search />
       </div>
     </div>
   );
