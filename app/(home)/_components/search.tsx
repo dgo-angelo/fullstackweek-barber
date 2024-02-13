27fd47ec53a737253386b68ada01af6ee6728f7a
Diego de Angelo <dgo.angelo@gmail.com>
2024-02-13 19:00:00 -0200

    (feat) add bookings page

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -6,7 +6,7 @@ import { SearchIcon } from 'lucide-react';
 const Search = () => {
   return (
     <div className='flex items-center gap-2'>
-      <Input placeholder='Busque po uma barbearia...' />
+      <Input placeholder='Busque por uma barbearia...' />
       <Button variant='default'>
         <SearchIcon size={20}></SearchIcon>
       </Button>
