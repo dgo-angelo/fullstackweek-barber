da63502890fc60701edc95a69da1d3b6a1979a5a
Diego de Angelo <dgo.angelo@gmail.com>
2024-02-13 19:00:00 -0200

    (feat) add welcome message

---
+++ app/(home)/page.tsx
@@ -0,0 +1,15 @@
+import { format } from 'date-fns';
+import Header from '../_components/header';
+import { ptBR } from 'date-fns/locale';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <div className='px-5 pt-5'>
+        <h2 className='text-xl font-bold'>Olá, Diego</h2>
+        <p className='capitalize'>{format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}</p>
+      </div>
+    </div>
+  );
+}
